da2bcc9b4c686002e34918793ea0fd0ff4eff62a
Brik-Max <151431543+Brik-Max@users.noreply.github.com>
2025-10-16 21:56:43 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 
 public class Main {
+
+    public static void yoFunc(){
+        System.out.println("Hello yoFunc");
+    }
+
+
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
